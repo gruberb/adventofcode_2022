dd177f1d12fa0af3fcfecebe9e12e05d2005c873
Bastian Gruber <foreach@me.com>
2022-12-04 11:20:09 +0100

    finish day_04

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+	fs::File,
+	io::{self, prelude::*, BufReader},
+};
+
+fn main() -> io::Result<()> {
+    let file = File::open("input.txt")?;
+	let reader = BufReader::new(file);
+
+	let mut part_1 = 0;
+    let mut part_2 = 0;
+
+	for line in reader.lines() {
+		let s = String::from_utf8(line?.into_bytes()).unwrap();
+        let ranges: Vec<&str> = s.split(",").collect();
+
+        let first_range: Vec<&str> = ranges[0].split("-").collect();
+        let second_range: Vec<&str> = ranges[1].split("-").collect();
+
+        let fd = first_range[0].parse::<i32>().unwrap();
+        let fu = first_range[1].parse::<i32>().unwrap();
+
+        let sd = second_range[0].parse::<i32>().unwrap();
+        let su = second_range[1].parse::<i32>().unwrap();
+
+        if fd <= sd && fu >= su || sd <= fd && su >= fu {
+            part_1 += 1;
+        }
+
+        let fr: Vec<i32> = (fd..=fu).collect();
+        let sr: Vec<i32> = (sd..=su).collect();
+
+        for i in fr.iter() {
+            println!("Check number {} in {:?}", i, sr);
+            if sr.contains(&i) {
+                println!("Number {:?} is in {:?}", i, sr);
+                part_2 += 1;
+                break;
+            }
+        }
+	}
+
+    println!("Part 1: {}", part_1);
+    println!("Part 2: {}", part_2);
+
+	Ok(())
 }
